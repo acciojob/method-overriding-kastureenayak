d83a250f8e45f4d64c2fc3a881d8c6a150bab4e6
Kasturee Nayak <kasturee1998@gmail.com>
2024-03-31 02:12:51 +0530

    Solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,7 +1,7 @@
 package com.driver;
 
 public class Main {
-  public static void main(String args[]){
+  public static void main(String[] args){
       B b =new B();
       String string=b.meth();
       System.out.println(string);
