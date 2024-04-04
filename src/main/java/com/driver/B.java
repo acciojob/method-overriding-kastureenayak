49204ade33b75eab44106a517d2718fbdad2de84
Kasturee Nayak <kasturee1998@gmail.com>
2024-04-04 21:50:05 +0530

    Overriding

--- src/main/java/com/driver/B.java
+++
@@ -1,9 +0,0 @@
-package com.driver;
-
-public class B extends A {
-    @java.lang.Override
-    public String meth() {
-
-        return  "Method is overridden in Extended class B";
-    }
-}
--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,9 +1,24 @@
+
 package com.driver;
 
 public class Main {
-  public static void main(String[] args){
-      B b =new B();
-      String string=b.meth();
-      System.out.println(string);
-  }
+    public static class A{
+        public String meth(){
+            return "Invoking method from class A";
+        }
+    }
+    public static class B extends A{
+
+        @Override
+        public String meth(){
+            return "Method is overridden in Extendend class B";
+        }
+    }
+    public static void main(String[] args){
+        A a = new A();
+        a.meth();
+
+        B b = new B();
+        b.meth();
+    }
 }
